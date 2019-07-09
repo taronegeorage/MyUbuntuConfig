da51f788a9250b8e0d4a23c3aa7aa148b1eae788
ZHAO, Jiaqi <603875085@qq.com>
2019-07-09 16:24:26 +0800

    Create install.sh

---
+++ ShadowSocks/install.sh
@@ -0,0 +1,4 @@
+#https://github.com/shadowsocks/shadowsocks-qt5/releases
+wget https://github.com/shadowsocks/shadowsocks-qt5/releases/download/v3.0.0/Shadowsocks-Qt5-3.0.0-x86_64.AppImage
+chmod a+x Shadowsocks-Qt5-3.0.0-x86_64.AppImage
+#./Shadowsocks-Qt5-3.0.0-x86_64.AppImage
