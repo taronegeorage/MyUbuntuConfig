6bb62863d0fb8445c0eeb97d3683e14f62a84f1d
ZHAO, Jiaqi <603875085@qq.com>
2019-07-09 17:25:51 +0800

    Update install.sh

--- ShadowSocks/install.sh
+++ ShadowSocks/install.sh
@@ -1,4 +1,8 @@
-#https://github.com/shadowsocks/shadowsocks-qt5/releases
+# https://github.com/shadowsocks/shadowsocks-qt5/releases
 wget https://github.com/shadowsocks/shadowsocks-qt5/releases/download/v3.0.0/Shadowsocks-Qt5-3.0.0-x86_64.AppImage
 chmod a+x Shadowsocks-Qt5-3.0.0-x86_64.AppImage
-#./Shadowsocks-Qt5-3.0.0-x86_64.AppImage
+# to open it: ./Shadowsocks-Qt5-3.0.0-x86_64.AppImage
+
+# proxy - SwitchOmega
+# https://github.com/FelisCatus/SwitchyOmega/files/3059599/proxy-switchyomega_2_5_20_0.zip
+# add a new proxy profile for Chorme
