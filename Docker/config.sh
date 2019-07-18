9b357b8702fe8c74cb39bd3aedab35d7cfadebf5
ZHAO, Jiaqi <603875085@qq.com>
2019-07-18 15:11:37 +0800

    Create config.sh

---
+++ Docker/config.sh
@@ -0,0 +1,11 @@
+# pytorch 0.4.1, cuda 9.0
+docker pull anibali/pytorch:cuda-9.0
+
+#caffe
+docker pull bvlc/caffe:gpu
+
+# tensorflow
+docker pull tensorflow/tensorflow:1.10.1-gpu
+
+# cuda
+docker pull nvidia/cuda:10.0-cudnn7-devel-ubuntu16.04
